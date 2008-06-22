9b76bd646bc332cae962a139206529e3d815abb7
gkiagia <gkiagia@6510684c-2033-0410-8336-a944b76521fa>
2008-06-22 17:25:20 +0000

    Implement an editor widget for WineDllOverrides.
    git-svn-id: https://qtwine.svn.sourceforge.net/svnroot/qtwine/trunk@154 6510684c-2033-0410-8336-a944b76521fa

---
+++ qtwine/src/widgets/winedlloverridesedit.cpp
@@ -0,0 +1,192 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by George Kiagiadakis <gkiagia@users.sf.net>       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+#include "winedlloverridesedit.h"
+#include <QPair>
+#include <QList>
+#include <QLinkedList>
+#include <QSignalMapper>
+#include <QVBoxLayout>
+#include <QResizeEvent>
+#include <KLineEdit>
+#include <KComboBox>
+#include <KPushButton>
+#include <KStandardGuiItem>
+#include <KHBox>
+#include <KLocalizedString>
+
+using namespace QtWine;
+
+class Line : public KHBox
+{
+public:
+    Line(const QPair<QString, WineDllOverrides::DllOverrideType> & o, QWidget *parent);
+
+    QPair<QString, WineDllOverrides::DllOverrideType> override() const;
+
+    KLineEdit *lineEdit;
+    KPushButton *rmButton;
+
+private:
+    void setupUi();
+    KComboBox *comboBox;
+};
+
+Line::Line(const QPair<QString, WineDllOverrides::DllOverrideType> & o, QWidget *parent)
+    : KHBox(parent)
+{
+    setupUi();
+    if ( !o.first.isEmpty() ) {
+        lineEdit->setText(o.first);
+        comboBox->setCurrentIndex(static_cast<int>(o.second));
+    } else
+        lineEdit->setClickMessage(i18n("Enter dll name here"));
+}
+
+QPair<QString, WineDllOverrides::DllOverrideType> Line::override() const
+{
+    QPair<QString, WineDllOverrides::DllOverrideType> p;
+    p.first = lineEdit->text();
+    p.second = static_cast<WineDllOverrides::DllOverrideType>(comboBox->currentIndex());
+    return p;
+}
+
+void Line::setupUi()
+{
+    lineEdit = new KLineEdit(this);
+    comboBox = new KComboBox(this);
+    comboBox->addItem(i18n("Disabled"));
+    comboBox->addItem(i18n("Native"));
+    comboBox->addItem(i18n("Builtin"));
+    comboBox->addItem(i18n("Try first native, then builtin"));
+    comboBox->addItem(i18n("Try first builtin, then native"));
+    rmButton = new KPushButton(KStandardGuiItem::remove(), this);
+}
+
+struct WineDllOverridesEditPrivate
+{
+    QWidget *centralWidget;
+    QList<Line*> lines;
+    QSignalMapper rmButtonsMapper;
+    KPushButton *addButton;
+    QVBoxLayout *layout;
+};
+
+
+WineDllOverridesEdit::WineDllOverridesEdit(QWidget *parent)
+    : QScrollArea(parent), d(new WineDllOverridesEditPrivate)
+{
+    d->centralWidget = new QWidget(this);
+    setWidget(d->centralWidget);
+    d->layout = new QVBoxLayout(d->centralWidget);
+    d->addButton = new KPushButton(KStandardGuiItem::add(), this);
+    d->layout->addWidget(d->addButton);
+    connect(&d->rmButtonsMapper, SIGNAL(mapped(QWidget*)), this, SLOT(removeLine(QWidget*)) );
+    connect(d->addButton, SIGNAL(clicked()), this, SLOT(addEmptyLine()) );
+}
+
+WineDllOverridesEdit::~WineDllOverridesEdit()
+{
+    delete d;
+}
+
+QString WineDllOverridesEdit::dllOverridesString() const
+{
+    return (QString) dllOverrides();
+}
+
+void WineDllOverridesEdit::setDllOverridesString(const QString & overrides)
+{
+    setDllOverrides(WineDllOverrides(overrides));
+}
+
+WineDllOverrides WineDllOverridesEdit::dllOverrides() const
+{
+    WineDllOverrides o;
+    foreach(Line *l, d->lines)
+        o << l->override();
+    return o;
+}
+
+void WineDllOverridesEdit::setDllOverrides(const WineDllOverrides & overrides)
+{
+    // delete previous entries
+    while ( !d->lines.isEmpty() ) {
+        Line *line = d->lines.takeFirst();
+        d->layout->removeWidget(line);
+        line->deleteLater();
+    }
+
+    // load new entries
+    QLinkedList< QPair<QString, WineDllOverrides::DllOverrideType> > list;
+    QLinkedList< QPair<QString, WineDllOverrides::DllOverrideType> >::const_iterator i;
+    list = overrides.overridesList();
+    i = list.constBegin();
+    while ( i != list.constEnd() ) {
+        addLine(*i);
+        ++i;
+    }
+}
+
+void WineDllOverridesEdit::addEmptyLine()
+{
+    addLine(qMakePair(QString(), WineDllOverrides::Disabled));
+}
+
+void WineDllOverridesEdit::addLine(const QPair<QString, WineDllOverrides::DllOverrideType> & o)
+{
+    Line *newLine = new Line(o, d->centralWidget);
+    d->lines.append(newLine);
+    d->layout->insertWidget(d->lines.size()-1, newLine);
+    d->rmButtonsMapper.setMapping(newLine->rmButton, newLine);
+    connect(newLine->rmButton, SIGNAL(clicked()), &d->rmButtonsMapper, SLOT(map()));
+    connect(newLine->lineEdit, SIGNAL(textChanged(QString)), this, SIGNAL(overridesChanged()) );
+    QMetaObject::invokeMethod(this, "resizeCentralWidget", Qt::QueuedConnection);
+}
+
+void WineDllOverridesEdit::removeLine(QWidget *line)
+{
+    d->lines.removeAt(d->lines.indexOf(static_cast<Line*>(line)));
+    d->layout->removeWidget(line);
+    line->deleteLater();
+    QMetaObject::invokeMethod(this, "resizeCentralWidget", Qt::QueuedConnection);
+    emit overridesChanged();
+}
+
+void WineDllOverridesEdit::resizeEvent(QResizeEvent *event)
+{
+    resizeCentralWidget();
+    QScrollArea::resizeEvent(event);
+}
+
+void WineDllOverridesEdit::resizeCentralWidget()
+{
+    QSize s = viewport()->size();
+    QSize wSize = d->centralWidget->size().expandedTo(s).boundedTo(s);
+    wSize.setHeight(d->layout->minimumSize().height());
+    d->centralWidget->resize(wSize);
+    ensureWidgetVisible(d->addButton);
+}
+
+QSize WineDllOverridesEdit::minimumSizeHint() const
+{
+    QSize s(460, d->addButton->minimumSize().height());
+    return s;
+}
+
+#include "winedlloverridesedit.moc"
---
+++ qtwine/src/widgets/winedlloverridesedit.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by George Kiagiadakis <gkiagia@users.sf.net>       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+#ifndef WINEDLLOVERRIDESEDIT_H
+#define WINEDLLOVERRIDESEDIT_H
+
+#include <QScrollArea>
+#include <qtwine/winedlloverrides.h>
+template <class T1, class T2> class QPair;
+class WineDllOverridesEditPrivate;
+
+class WineDllOverridesEdit : public QScrollArea
+{
+    Q_OBJECT
+    /* normally, the DllOverrides property should be the USER property
+     * but in the program shortcut editor, we need to use a QDataWidgetMapper
+     * to assign a string to this widget, so the USER property must be a QString.
+     */
+    Q_PROPERTY(QString dllOverridesString READ dllOverridesString WRITE setDllOverridesString USER true)
+  //Q_PROPERTY(QtWine::WineDllOverrides dllOverrides READ dllOverrides WRITE setDllOverrides) //FIXME moc bug?
+public:
+    WineDllOverridesEdit(QWidget *parent = 0);
+    ~WineDllOverridesEdit();
+
+    QString dllOverridesString() const;
+    void setDllOverridesString(const QString & overrides);
+
+    QtWine::WineDllOverrides dllOverrides() const;
+    void setDllOverrides(const QtWine::WineDllOverrides & overrides);
+
+    virtual QSize minimumSizeHint() const;
+
+protected:
+    virtual void resizeEvent(QResizeEvent *event);
+
+Q_SIGNALS:
+    void overridesChanged();
+
+private Q_SLOTS:
+    void addEmptyLine();
+    void addLine(const QPair<QString, QtWine::WineDllOverrides::DllOverrideType> & o);
+    void removeLine(QWidget *line);
+    void resizeCentralWidget();
+
+private:
+    WineDllOverridesEditPrivate *d;
+};
+
+
+#endif
