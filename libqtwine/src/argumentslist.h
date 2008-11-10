d60e6b30dcfdb973375e5a86bc279c9c0d3c62a5
George Kiagiadakis <gkiagia@users.sourceforge.net>
2008-11-11 00:35:52 +0200

    ifndef ARGUMENTSLIST_H -> ifndef _QTWINE_ARGUMENTSLIST_H (prepend namespace name)

--- libqtwine/src/argumentslist.h
+++ libqtwine/src/argumentslist.h
@@ -14,8 +14,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef ARGUMENTSLIST_H
-#define ARGUMENTSLIST_H
+#ifndef _QTWINE_ARGUMENTSLIST_H
+#define _QTWINE_ARGUMENTSLIST_H
 
 #include "libqtwine_export.h"
 #include "libqtwine_global.h"
